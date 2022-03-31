56eea14019ae4994b5fa39308fec909f1eb99c16
zhangyufei <andryyu5419@gmail.com>
2022-03-31 18:02:10 +0800

    升升级配置

---
+++ app/src/main/java/com/andryyu/kthand/ui/activity/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.andryyu.kthand.ui.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.andryyu.kthand.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
