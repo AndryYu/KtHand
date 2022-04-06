a6fe05245925fbae733e6f6e781b8361c54f5b1c
zhangyufei <andryyu5419@gmail.com>
2022-04-06 16:17:49 +0800

    gradle 组件化配置

---
+++ ComponentOneWen/src/main/java/com/andryyu/onewen/OneWenDetailActivity.kt
@@ -0,0 +1,11 @@
+package com.andryyu.onewen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OneWenDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_one_wen_detail)
+    }
+}
